0076e47b98fbd01a42e50099bd3901907b3603e2
lonqdh <longndhgch210216@fpt.edu.vn>
2024-04-08 13:22:27 +0700

    testlong

--- back-end/UnitTest/EventControllerTest.cs
+++ back-end/UnitTest/EventControllerTest.cs
@@ -1,4 +1,12 @@
-﻿using NUnit.Framework;
+﻿using AutoMapper;
+using Comp1640_Final.Controllers;
+using Comp1640_Final.DTO.Request;
+using Comp1640_Final.Models;
+using Comp1640_Final.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +15,133 @@ using System.Threading.Tasks;
 
 namespace UnitTest
 {
-    public class EventControllerTest
+    [TestFixture]
+    public class EventControllerTests
     {
+        private EventsController eventsController;
+        private Mock<IEventService> eventServiceMock;
+        private IMapper mapper;
+        private Mock<UserManager<ApplicationUser>> userManagerMock;
 
+        [SetUp]
+        public void Setup()
+        {
+            eventServiceMock = new Mock<IEventService>();
+            mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Event, EventDTO>().ReverseMap();
+            }).CreateMapper();
+
+            userManagerMock = new Mock<UserManager<ApplicationUser>>(Mock.Of<IUserStore<ApplicationUser>>(),
+                null, null, null, null, null, null, null, null);
+            eventsController = new EventsController(null, mapper, userManagerMock.Object, eventServiceMock.Object);
+        }
+
+        [Test]
+        public async Task GetEvents_ReturnsOkResult()
+        {
+            // Arrange
+            var events = new List<Event> {
+                new Event { Id = 1, EventName = "Long Hoang" },
+                new Event { Id = 2, EventName = "Good Boy"}
+            };
+            eventServiceMock.Setup(x => x.GetEvents()).Returns(events);
+
+            // Act
+            IActionResult result = await eventsController.GetEvents();
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        }
+
+        [Test]
+        public async Task GetStudentEvent_WithValidUserId_ReturnsOkResult()
+        {
+            // Arrange
+            var userId = "validUserId";
+            var user = new ApplicationUser { Id = userId, UserName = "testuser" };
+            var testEvent = new Event { Id = 1, EventName = "Event 1" };
+
+            userManagerMock.Setup(x => x.FindByIdAsync(userId)).ReturnsAsync(user);
+
+            eventServiceMock.Setup(x => x.GetFirstEventByFaculty(It.IsAny<int>())).ReturnsAsync(testEvent);
+
+            // Act
+            //var result = await _eventsController.GetStudentEvent(userId);
+            var actionResult = await eventsController.GetStudentEvent(userId);
+
+            // Assert
+            Assert.That(actionResult, Is.Not.Null);
+            Assert.That(actionResult.Result, Is.InstanceOf<OkObjectResult>());
+
+            // Extract the value from the ActionResult
+            var okObjectResult = actionResult.Result as OkObjectResult;
+            Assert.That(okObjectResult.Value, Is.InstanceOf<EventDTO>());
+        }
+
+        [Test]
+        public async Task PostEvent_WithValidEvent_ReturnsOkResult()
+        {
+            // Arrange
+            var eventDto = new EventDTO { Id = 1, EventName = "Test Event" };
+            var createdEvent = new Event { Id = 1, EventName = "Test Event" };
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(m => m.Map<Event>(eventDto)).Returns(createdEvent);
+
+            eventsController = new EventsController(null, mapperMock.Object, null, eventServiceMock.Object);
+
+            eventServiceMock.Setup(x => x.CreateEvent(createdEvent)).ReturnsAsync(true);
+
+            // Act
+            var result = await eventsController.PostEvent(eventDto);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+
+            //// Arrange
+            //var eventDto = new EventDTO { Id = 1, EventName = "Test Event" };
+            //var createdEvent = new Event { Id = 1, EventName = "Test Event" };
+
+            //eventServiceMock.Setup(x => x.CreateEvent(It.IsAny<Event>())).ReturnsAsync(true);
+
+            //var mapperMock = new Mock<IMapper>();
+            //mapperMock.Setup(m => m.Map<Event>(eventDto)).Returns(createdEvent);
+
+            ////mapper.Setup(m => m.Map<Event>(eventDto)).Returns(createdEvent);
+
+            //// Act
+            //var result = await eventsController.PostEvent(eventDto);
+
+            //// Assert
+            //Assert.That(result, Is.Not.Null);
+            //Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+        }
+
+
+        [Test]
+        public async Task Put_WithValidEvent_ReturnsOkResult()
+        {
+            // Arrange
+            int eventId = 1;
+            var eventDto = new EventDTO { Id = eventId, EventName = "Updated Event" };
+            var updatedEvent = new Event { Id = eventId, EventName = "Updated Event" };
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(m => m.Map<Event>(eventDto)).Returns(updatedEvent);
+
+            eventsController = new EventsController(null, mapperMock.Object, null, eventServiceMock.Object);
+
+            eventServiceMock.Setup(x => x.EditEvent(updatedEvent)).ReturnsAsync(true);
+
+            // Act
+            var result = await eventsController.Put(eventId, eventDto);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Result, Is.InstanceOf<OkObjectResult>());
+        }
     }
 }
---
+++ back-end/UnitTest/RolesControllerTest.cs
@@ -0,0 +1,171 @@
+﻿using Comp1640_Final.Controllers;
+using Comp1640_Final.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestFixture]
+    [TestFixture]
+    public class RolesControllerTest
+    {
+        private RolesController _rolesController;
+        private Mock<RoleManager<IdentityRole>> _roleManagerMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _roleManagerMock = new Mock<RoleManager<IdentityRole>>(new Mock<IRoleStore<IdentityRole>>().Object, null, null, null, null);
+
+            _rolesController = new RolesController(
+                _roleManagerMock.Object
+            );
+        }
+
+        [Test]
+        public async Task PostRole_ReturnsOkResult()
+        {
+            // Arrange
+            var role = new Role { RoleName = "TestRole" };
+            var identityResult = IdentityResult.Success;
+
+            _roleManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityRole>())).ReturnsAsync(identityResult);
+
+            // Act
+            var result = await _rolesController.PostRole(role);
+
+            // Assert
+            Assert.That(result, Is.Not.Null.And.InstanceOf<OkObjectResult>());
+            var okResult = result as OkObjectResult;
+            Assert.That(okResult.Value, Is.EqualTo(role.RoleName));
+        }
+
+        [Test]
+        public async Task PostRole_ReturnsBadRequest_WhenCreateRoleFails()
+        {
+            // Arrange
+            var role = new Role { RoleName = "TestRole" };
+            var identityResult = IdentityResult.Failed(new IdentityError { Description = "Failed to create role" });
+
+            _roleManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityRole>())).ReturnsAsync(identityResult);
+
+            // Act
+            var result = await _rolesController.PostRole(role);
+
+            // Assert
+            Assert.That(result, Is.Not.Null.And.InstanceOf<BadRequestResult>());
+
+        }
+
+        //[Test]
+        //public async Task GetRole_ReturnsOkResult_WithRoleList()
+        //{
+        //    // Arrange
+        //    var roles = new List<IdentityRole>
+        //{
+        //    new IdentityRole { Name = "Role1" },
+        //    new IdentityRole { Name = "Role2" }
+        //};/*.AsQueryable();*/   
+
+        //    // Mock the Roles property to return an IQueryable asynchronously
+        //    _roleManagerMock.Setup(x => x.Roles).Returns(roles.AsQueryable);
+
+        //    // Act
+        //    var result = await _rolesController.GetRole();
+
+        //    // Assert
+        //    Assert.That(result, Is.Not.Null.And.InstanceOf<OkObjectResult>());
+        //    Assert.That(result.Value, Is.InstanceOf<IEnumerable<IdentityRole>>());
+        //    var returnedRoles = result.Value as IEnumerable<IdentityRole>;
+        //    Assert.That(returnedRoles.Count(), Is.EqualTo(roles.Count()));
+        //}
+
+
+
+        ////////////
+        ///
+
+        //[Test]
+        //public async Task GetRole_ReturnsInternalServerError_WhenRoleRetrievalFails()
+        //{
+        //    // Arrange
+        //    _roleManagerMock.Setup(x => x.Roles).Throws(new Exception("Failed to retrieve roles"));
+
+        //    // Act
+        //    var result = await _rolesController.GetRole();
+
+        //    // Assert
+        //    Assert.That(result, Is.Not.Null.And.InstanceOf<ObjectResult>());
+        //    var objectResult = result as ObjectResult;
+        //    Assert.That(objectResult.StatusCode, Is.EqualTo(StatusCodes.Status500InternalServerError));
+        //}
+
+
+        [Test]
+        public async Task PostRole_ReturnsInternalServerError_WhenRoleManagerFails()
+        {
+            // Arrange
+            var role = new Role { RoleName = "TestRole" };
+            var identityResult = IdentityResult.Failed(new IdentityError { Description = "Failed to create role" });
+
+            _roleManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityRole>())).ReturnsAsync(identityResult);
+
+            // Act
+            var result = await _rolesController.PostRole(role);
+
+            // Assert
+            Assert.That(result, Is.Not.Null.And.InstanceOf<StatusCodeResult>());
+            var statusCodeResult = result as StatusCodeResult;
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+            //Assert.That(statusCodeResult.StatusCode, Is.EqualTo(StatusCodes.Status500InternalServerError));
+        }
+
+        //[Test]
+        //public async Task PostRole_ReturnsBadRequest_WhenRoleNameIsInvalid()
+        //{
+        //    // Arrange
+        //    var role = new Role { RoleName = null }; // Invalid role name
+
+        //    // Act
+        //    var result = await _rolesController.PostRole(role);
+
+        //    // Assert
+        //    Assert.That(result, Is.Not.Null.And.InstanceOf<StatusCodeResult>());
+        //    var statusCodeResult = result as StatusCodeResult;
+        //    Assert.That(statusCodeResult.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+        //}
+
+        [Test]
+        public async Task PostRole_ReturnsInternalServerError_WhenRoleCreationFails()
+        {
+            // Arrange
+            var role = new Role { RoleName = "TestRole" };
+            var identityResult = IdentityResult.Failed(new IdentityError { Description = "Failed to create role" });
+
+            _roleManagerMock.Setup(x => x.CreateAsync(It.IsAny<IdentityRole>())).ReturnsAsync(identityResult);
+
+            // Act
+            var result = await _rolesController.PostRole(role);
+
+            // Assert
+            Assert.That(result, Is.Not.Null.And.InstanceOf<StatusCodeResult>());
+            var statusCodeResult = result as StatusCodeResult;
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+        }
+
+    }
+
+}
